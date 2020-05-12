38b7e810e4484f479da9572b277b38b16da45dda
joshbm-git <32262804+joshbm-git@users.noreply.github.com>
2020-05-12 18:38:59 +0200

    Add files via upload

---
+++ precache-manifest.4cd71263765c1cc51804bc77dd20bef3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6eedc46dfba756dac8c",
+    "url": "/css/app.8d23bc70.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "059efff9e3356d3014c98a2f27389212",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d639d69b3acbd4a04d1b",
+    "url": "/js/about.dc2bbb83.js"
+  },
+  {
+    "revision": "a6eedc46dfba756dac8c",
+    "url": "/js/app.ee4714ff.js"
+  },
+  {
+    "revision": "19e78f97c613afd61629",
+    "url": "/js/chunk-vendors.00092a4d.js"
+  },
+  {
+    "revision": "47cab5286ec984223317bbdeb27ba076",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
